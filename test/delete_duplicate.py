cc79826472a6a39167145694fdd2bebe3d8671f1
jioby <jioby@example.com>
2024-02-23 19:22:30 +0900

    refactor: 不使用正则提取

--- test/delete_duplicate.py
+++ test/delete_duplicate.py
@@ -2,15 +2,14 @@
 删除rule.txt文件中Unicode码一样的汉字，并按照一定的规律排序
 """
 
-import re
-
 with open("temp.txt", encoding="UTF-8") as input_file, open(
     "save.txt", "w", encoding="utf-8"
 ) as output_file:
     output_group = set()
-    REG = re.compile(r"(\w*) (\w*)")
-    for iter_ in REG.finditer(input_file.read()):
-        # 判断Unicode码是否一致
-        if iter_.group(1) != iter_.group(2):
-            output_group.add(iter_.group(0) + "\n")
+    for line in input_file:
+        columns = line.strip().split(" ")
+        if len(columns) >= 2 and columns[0] != columns[1]:
+            output_group.add(line)
+        else:
+            print(f"{line} 没有空格作为分隔符，请注意检查！")
     output_file.writelines(sorted(output_group))
