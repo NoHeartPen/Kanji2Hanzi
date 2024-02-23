d3d05d157c3e6784bc8e30bf01b87254cf90d6dc
jioby <jioby@example.com>
2024-02-23 18:52:51 +0900

    fix: 更换提取规则分割标识符

--- test/delete_duplicate.py
+++ test/delete_duplicate.py
@@ -8,7 +8,7 @@ with open("temp.txt", encoding="UTF-8") as input_file, open(
     "save.txt", "w", encoding="utf-8"
 ) as output_file:
     output_group = set()
-    REG = re.compile(r"(\w*)\t(\w*)")
+    REG = re.compile(r"(\w*) (\w*)")
     for iter_ in REG.finditer(input_file.read()):
         # 判断Unicode码是否一致
         if iter_.group(1) != iter_.group(2):
