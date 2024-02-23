1ba6712a62f7daa997c07a4dfb2464903157a93a
jioby <jioby@example.com>
2024-02-23 18:49:51 +0900

    refactor: pylint 规范

--- test/delete_duplicate.py
+++ test/delete_duplicate.py
@@ -1,13 +1,16 @@
-'''
+"""
 删除rule.txt文件中Unicode码一样的汉字，并按照一定的规律排序
-'''
+"""
 
 import re
-with open("temp.txt", encoding="UTF-8") as InputFile, open("save.txt", "w", encoding="utf-8")as OutputFile:
-    OutputTextGroup = set()
-    Reg = re.compile(r"(\w*)\t(\w*)")
-    for iter in Reg.finditer(InputFile.read()):
-        print(iter.group(1))
-        if iter.group(1) != iter.group(2):  # 判断Unicode码是否一致
-            OutputTextGroup.add(iter.group(0)+'\n')
-    OutputFile.writelines(sorted(OutputTextGroup))
+
+with open("temp.txt", encoding="UTF-8") as input_file, open(
+    "save.txt", "w", encoding="utf-8"
+) as output_file:
+    output_group = set()
+    REG = re.compile(r"(\w*)\t(\w*)")
+    for iter_ in REG.finditer(input_file.read()):
+        # 判断Unicode码是否一致
+        if iter_.group(1) != iter_.group(2):
+            output_group.add(iter_.group(0) + "\n")
+    output_file.writelines(sorted(output_group))
